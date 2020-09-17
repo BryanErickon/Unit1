14e87c9ea91eb2ed9c14f3fa1c59dfbc8ef1433a
BryanErickon <817645@mystma.org>
2020-09-17 16:28:45 +0000

    I did the work

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,33 +2,12 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int myFavNum = 456;
-        byte states = 49;
-        char rtname = 'D';
-        short waterBoil = 212;
-        long lightSpeed = 299792458;
-        float tesla = 69990;
-        double usDebt = 14565536;
-        boolean myCoolness = true;  
+        String firstName = "Bryan";
+        String lastName = "Erickson";
 
-        System.out.println("What number do I like?");
-        System.out.println(myFavNum);
-        System.out.println("How many staes exist?");
-        System.out.println(states);
-        System.out.println("i dont recognize iowa");
-        System.out.println(" what is the letter of Rt's real name? ");
-        System.out.println(rtname);
-        System.out.println("When does water boil");
-        System.out.println(waterBoil);
-        System.out.println("what is the speed of light?");
-        System.out.println(lightSpeed);
-        System.out.println("What is the price of a tesla model Y?");
-        System.out.println(tesla);
-        System.out.println("How much debt does the US have?");
-        System.out.println(usDebt);
-        System.out.println("am I cool?");
-        System.out.println(myCoolness);
+        System.out.println(firstName + " " + lastName);
 
+        
 
     }
 }
