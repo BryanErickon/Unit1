7e9bb30258e0844c26ee53ca020c13a08ba9e34f
BryanErickon <817645@mystma.org>
2020-09-22 20:21:50 +0000

    Work on tommorrow

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,43 +2,26 @@ package app;
 import java.util.Scanner;
 
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-      
-      Scanner noun = new Scanner(System.in);
 
-      System.out.println("Please enter a proper noun");
-      String noun2 = noun.nextLine();
-      System.out.println("this is a " + noun2);
+        Scanner color = new Scanner(System.in);
+        System.out.println("Please enter a color");
+        String color2 = color.nextLine();
+        System.out.println("You have chosen " + color2 + " as your color");
 
-      Scanner adj = new Scanner(System.in);
-      System.out.println("Please enter an adjective");
-      String adj2 = adj.nextLine();
-      System.out.println("the " + noun2 + " is " + adj2);
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter a number between 0-9");
+        int number2 = number.nextInt();
+        System.out.println("The number you have chose is " + number2);
 
-      Scanner place = new Scanner(System.in);
-      System.out.println("enter a place");
-      String place2 = place.nextLine();
-      System.out.println("Your place is " + place2);
+        while (number2 < 9) {
+            System.out.println(color2);
+            number2 = number2 + 1;
+        }
 
-      Scanner verb = new Scanner(System.in);
-      System.out.println("Please enter a past tense verb");
-      String verb2 = verb.nextLine();
-      System.out.println("Your verb is " + verb2);
-
-      Scanner food = new Scanner(System.in);
-      System.out.println("Please enter a food");
-      String food2 = food.nextLine();
-      System.out.println("Your food is " + food2);
-
-      System.out.println("One day, a " + adj2 + " " + noun2 + " " + "in " + place2 + " " + verb2 + " some " +  food2);
-
-
-
+        }
 
 
 
     }
-}
