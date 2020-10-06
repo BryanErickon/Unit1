d28ede91034337cb252abf62c311a8b464226540
BryanErickon <817645@mystma.org>
2020-10-06 19:51:58 +0000

    Challenge 2 completed

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,77 +6,64 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-Scanner password = new Scanner(System.in);
-System.out.println("What's the password?");
-String input = password.nextLine();
-String correct = "Tacos";
-int failCount = 3;
-int fail = 4;
+        Scanner password = new Scanner(System.in);
+        System.out.println("What's the password?");
+        String input = password.nextLine();
+        String correct = "Tacos";
+        int failCount = 3;
+        boolean right = false;
 
-if (input.equals(correct)) {
-    System.out.println("Password accepted");
-} else {
-    do {
-        failCount--;
-        System.out.println("You have gotten the password wrong, you have "  + failCount + " tries left");
-        fail++;
-        Scanner password2 = new Scanner(System.in);
-        System.out.println("Please try again");
-        String input2 = password2.nextLine();
-        if (input2.equals(correct)) {
-     System.out.println("That is correct");
- 
-    }}
-  while (fail <= 3); 
- Scanner password3 = new Scanner(System.in);
- System.out.println("Enter password again");
- String input3 = password3.nextLine();
- 
- if (input3.equals(correct)) {
-     System.out.println("Password accepted");
- } else {
-     do {
-         failCount--;
-        System.out.println("You have gotten the password wrong, you have "  + failCount + " tries left");
-        fail++;
-
-
-     }
-     while (fail <= 2);
- }
-  
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-}
-    
-
+        if (input.equals(correct)) {
+            System.out.println("Password accepted");
+            System.out.println("Welcome to our club! Welcome to our club! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squid...!");
+        } else {
+            do {
+                failCount--;
+                System.out.println("You have gotten the password wrong, you have "  + failCount + " tries left");
+               
+                Scanner password2 = new Scanner(System.in);
+                System.out.println("Please try again");
+                String input2 = password2.nextLine();
+                if (input2.equals(correct)) {
+                    System.out.println("Password accepted, Welcome to our club! Welcome to our club! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squid...!");
+                    right = true;
+                } 
+            } while (failCount >= 2 && failCount <= 3 && !right);
+        }
     }
-
 }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+    
+
+
 
 
 
