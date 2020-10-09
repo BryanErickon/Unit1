ade937095de31052151be0270b3e3b92ad004183
BryanErickon <817645@mystma.org>
2020-10-09 13:36:33 +0000

    Challenge 3, not yet done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -13,21 +13,20 @@ public class App {
         System.out.println("Please enter a number between 0 and 100");
         int input = number.nextInt();
     
-        if (input == 59) {
+        if (input == 59 || input2 == 59 || input3 == 59) {
             System.out.println("Correct!");
         } else { 
-            do {
-                if (input <= 58 || input2 <= 58 || input3 <=58) {
-                    System.out.println("Too low, try again");
-                      int input2 = number.nextInt();
-                } else if (input >= 60 || input2 >= 60 || input3 >= 60) {
-                    System.out.println("Too high, try again");
+            while (!(input == 59 || input2 == 59 || input3 ==59)){
+                if (input <= 58 || input2 <= 58 || input3 <=58){
+                    System.out.println("Too low, please try again.");
+                    int input2 = number.nextInt();
+                } else if (input >= 60 || input2 >= 60 || input3 >=60) {
+                    System.out.println("Too high, try again.");
                     int input3 = number.nextInt();
-                } else if (input2 == 59 || input3 == 59) {
-                    System.out.println("Correct, you did it!");
                 }
 
-            } while ( input2 < 59 || input3 > 59);
+
+            }
         }
             
         
