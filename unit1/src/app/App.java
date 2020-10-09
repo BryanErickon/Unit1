e02bf1b4f647705ff70330b3c6ba0df1ae316afb
BryanErickon <817645@mystma.org>
2020-10-09 02:18:25 +0000

    Challenge 3, not done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 
 
 public class App {
+    private static int input2;
+    private static int input3;
+
     public static void main(String[] args) throws Exception {
 
-        Scanner password = new Scanner(System.in);
-        System.out.println("What's the password?");
-        String input = password.nextLine();
-        String correct = "Tacos";
-        int failCount = 3;
-        boolean right = false;
-
-        if (input.equals(correct)) {
-            System.out.println("Password accepted");
-            System.out.println("Welcome to our club! Welcome to our club! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squid...!");
-        } else {
+        Scanner number = new Scanner(System.in);
+        System.out.println("Please enter a number between 0 and 100");
+        int input = number.nextInt();
+    
+        if (input == 59) {
+            System.out.println("Correct!");
+        } else { 
+            
             do {
-                failCount--;
-                System.out.println("You have gotten the password wrong, you have "  + failCount + " tries left");
-               
-                Scanner password2 = new Scanner(System.in);
-                System.out.println("Please try again");
-                String input2 = password2.nextLine();
-                if (input2.equals(correct)) {
-                    System.out.println("Password accepted, Welcome to our club! Welcome to our club! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squidward! Welcome Squid...!");
-                    right = true;
-                } 
-            } while (failCount >= 2 && failCount <= 3 && !right);
+                if (input <= 58 || input2 <= 58 || input3 <=58) {
+                    System.out.println("Too low, try again");
+                      int input2 = number.nextInt();
+                 
+                } else if (input >= 60 || input2 >= 60 || input3 >= 60) {
+                    System.out.println("Too high, try again");
+                    int input3 = number.nextInt();
+                } else if (input2 == 59 || input3 == 59) {
+                    System.out.println("Correct, you did it!");
+                }
+
+            } while ( input2 < 59 || input3 > 59);
         }
+            
+        
+
+
+
+
+
+
+
     }
 }
 
