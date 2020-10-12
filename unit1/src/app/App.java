0dda76d09a5d8b6c5fcd58b3c3d8019f5e2b13ce
BryanErickon <817645@mystma.org>
2020-10-12 19:36:27 +0000

    Challenge 3 done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,82 +1,36 @@
 package app;
+
 import java.util.Scanner;
 
-
-
 public class App {
-    private static int input2;
-    private static int input3;
 
     public static void main(String[] args) throws Exception {
 
+         int target = 58;
+        boolean isFinished = false;
+        while (isFinished == false) {
+
         Scanner number = new Scanner(System.in);
-        System.out.println("Please enter a number between 0 and 100");
+        System.out.println("Please guess a number between 0 and 100");
         int input = number.nextInt();
-        int target = 58;
+        number.nextLine();
+       
 
-        if (input == target) {
-             System.out.println("Correct");
-        } else if (input < target) {
-            System.out.println("Too low, try again");
-        } else if (input > target) {
-            System.out.println("Too high, try again");
+        if (input == target){
+            System.out.println("Correct");
+            isFinished = true;
+        } else  {
+            if (input > target) {
+                System.out.println("Too high");
+            }
+                else {
+                    System.out.println("Too low");
+                }
+                
+             
+            } 
+            
+            }
         }
-        
-            
-        
 
-
-
-
-
-
-
-    }
 }
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-    
-
-    
-
-
-
-
-
-
-    
-
-        
-
-        
-           
-            
-            
-
-            
-
