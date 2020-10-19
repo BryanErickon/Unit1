49736ffa869ee23daf5f8ebce135bcde521c60cf
BryanErickon <817645@mystma.org>
2020-10-19 20:15:24 +0000

    Roulette challenge, day1

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,34 +1,21 @@
 package app;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-Scanner input1 = new Scanner(System.in);
-System.out.println("Please enter a number");
-int number1 = input1.nextInt();
-Scanner input2 = new Scanner(System.in);
-System.out.println("Please enter another number");
-int number2 = input2.nextInt();
 
-if (number1 < number2){
-for (number1++; number1 <= number2; number1++){
-    if (number1 %2 ==0){
-        System.out.println(number1);
-    }else {
+Random spin = new Random();
+int betNum = 10;
+int resultNum = spin.nextInt(37);
+int resultColor = 1 + spin.nextInt(3) -1;
 
-    }
-}
-}else {
-    for (number1--; number1 >= number2; number1--){
-        if (number1 %2 == 0){
-            System.out.println(number1);
-        }
-    }
-}
-    
+System.out.println(resultNum);
+System.out.println(resultColor);
+
 
 
 
