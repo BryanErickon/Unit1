dfaf313e728d643af10b42719b3b5d6bb555492c
BryanErickon <817645@mystma.org>
2020-10-20 20:15:49 +0000

    roulette cahllenge day 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -7,24 +7,40 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        Random spin = new Random();
+        int betNum = 10;
+        int resultNum = spin.nextInt(37);
+        int resultColor = 1 + spin.nextInt(3) - 1;
+        int betSum = 100;
+        String insideBet = "inside";
+        Scanner inorout = new Scanner(System.in);
+        String check = "yes";
+        
 
-Random spin = new Random();
-int betNum = 10;
-int resultNum = spin.nextInt(37);
-int resultColor = 1 + spin.nextInt(3) -1;
+        System.out.println("Would you like to make an inside bet or an outside bet?");
+        String inorout1 = inorout.nextLine();
 
-System.out.println(resultNum);
-System.out.println(resultColor);
+        if (inorout1.equals(insideBet)) {
+           do{
+                betNum--;
+                Scanner chosenNum = new Scanner(System.in);
+                System.out.println("What number so you want to bet on?");
+                String chosenNum1 = chosenNum.nextLine();
 
+                System.out.println("How much do you want to bet on " + chosenNum1);
+                Scanner bet = new Scanner(System.in);
+                String betAmount1 = bet.nextLine();
+                System.out.println("You have bet $" + betAmount1 + " on " + chosenNum1);
 
-
-
-
-
-
-
-
-}
+                System.out.println("You have " + betNum + " bets remaining. Would you like to bet again?"); 
+                Scanner again = new Scanner(System.in);
+                String again1 = again.nextLine();
+                String a = "yes";
+                if  (again1.equals(a)){
+                    boolean repeat = true;
+                }
+            } while (betNum > 0 && repeat == true );
+        }
 
 
 
@@ -41,4 +57,4 @@ System.out.println(resultColor);
 
     }
 
-        
+}
